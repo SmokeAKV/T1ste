a022cc09002a723463713b3b115117c489913daa
Smoke akv <149188903+SmokeAKV@users.noreply.github.com>
2025-01-04 21:08:34 -0300

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-clear
-while :
-do
-printf "\033[92m「 CONEXÃO 」Reinício automático. Iniciando Cecilia\033[0m\n"
-node ./main.js --code
-printf "\033[91m「 CONEXÃO 」Fechando programa...\033[0m\n"
-sleep 1
-done
